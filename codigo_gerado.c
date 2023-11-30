aaea82b9e7f2b193d1bb8404a095732b893c26ec
renatomneto <renatomneto@users.noreply.github.com>
2023-11-29 23:28:14 -0300

    <Feat> [Out]

--- codigo_gerado.c
+++ codigo_gerado.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 int main(){
-   int var1 = 1;
-   float var2 = 1.0;
-   bool var3 = true;
-   bool var4 = false;
-   char var5 = 'a';
+   printf("Hello World\n");
+   printf("Com duas linhas\n");
    
    return 0;
 }
